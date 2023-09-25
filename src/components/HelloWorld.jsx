b8a37cf38f595dfcdba81fabfcb601b4d1b4ac57
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-09-25 17:25:15 -0500

    :sparkles: create component, import and usage in the component main

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+const HelloWorld  = () => {
+    return (
+        <>
+            <h1>Hola mundo</h1>
+            <p>Que tal!</p>
+        </>
+    )
+}
+
+export default HelloWorld
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import HelloWorld from "./components/HelloWorld.jsx";
 
-const h1 = React.createElement('div', null, React.createElement("ol",null, React.createElement("li",null, "Item 1")))
 ReactDOM.createRoot(document.getElementById('root')).render(
-    h1
+    <HelloWorld />
 )
