276c9c286fb42a6bba4e9694646a11673e2bde3d
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-09-25 17:10:25 -0500

    :sparkles: modify example renders with react dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+const h1 = React.createElement('div', null, React.createElement("ol",null, React.createElement("li",null, "Item 1")))
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    h1
 )
